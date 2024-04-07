dc0a1a0a353c4d92bf20dab1de47d9a8ac054995
tanmayc05 <113726318+tanmayc05@users.noreply.github.com>
2024-04-07 16:08:56 -0500

    implement melody generator and output as a .mid file

--- melody_generator.py
+++ melody_generator.py
@@ -1,7 +1,8 @@
 import json
 import numpy as np
 from tensorflow import keras
-from preprocess import SEQUENCE_LENGTH, MAPPING_PATH
+import music21 as m21
+from preprocess import SEQUENCE_LENGTH, MAPPINGS_PATH
 
 class MelodyGenerator:
     """A class that wraps the LSTM model and offers utilities to generate melodies."""
@@ -12,7 +13,7 @@ class MelodyGenerator:
         self.model_path = model_path
         self.model = keras.models.load_model(model_path)
 
-        with open(MAPPING_PATH, "r") as fp:
+        with open(MAPPINGS_PATH, "r") as fp:
             self._mappings = json.load(fp)
 
         self._start_symbols = ["/"] * SEQUENCE_LENGTH
@@ -87,8 +88,59 @@ class MelodyGenerator:
         return index
 
 
+    def save_melody(self, melody, step_duration=0.25, format="midi", file_name="mel.mid"):
+        """Converts a melody into a MIDI file
+
+        :param melody (list of str):
+        :param min_duration (float): Duration of each time step in quarter length
+        :param file_name (str): Name of midi file
+        :return:
+        """
+
+        # create a music21 stream
+        stream = m21.stream.Stream()
+
+        start_symbol = None
+        step_counter = 1
+
+        # parse all the symbols in the melody and create note/rest objects
+        for i, symbol in enumerate(melody):
+
+            # handle case in which we have a note/rest
+            if symbol != "_" or i + 1 == len(melody):
+
+                # ensure we're dealing with note/rest beyond the first one
+                if start_symbol is not None:
+
+                    quarter_length_duration = step_duration * step_counter # 0.25 * 4 = 1
+
+                    # handle rest
+                    if start_symbol == "R":
+                        m21_event = m21.note.Rest(quarterLength=quarter_length_duration)
+
+                    # handle note
+                    else:
+                        m21_event = m21.note.Note(int(start_symbol), quarterLength=quarter_length_duration)
+
+                    stream.append(m21_event)
+
+                    # reset the step counter
+                    step_counter = 1
+
+                start_symbol = symbol
+
+            # handle case in which we have a prolongation sign "_"
+            else:
+                step_counter += 1
+
+        # write the m21 stream to a midi file
+        stream.write(format, file_name)
+
+
 if __name__ == "__main__":
     mg = MelodyGenerator()
-    seed = "55 _ _ _ 60 _ _ _ 55 _ _ _ 55 _"
-    melody = mg.generate_melody(seed, 500, SEQUENCE_LENGTH, 0.7)
+    seed = "67 _ 67 _ 67 _ _ 65 64 _ 64 _ 64 _ _"
+    seed2 = "67 _ _ 86 _ _ 65 _ 64 _ 62 _ 60 _ _ _"
+    melody = mg.generate_melody(seed2, 500, SEQUENCE_LENGTH, 0.3)
     print(melody)
+    mg.save_melody(melody)
